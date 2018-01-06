4319ca884668d56b27985ee60fd876357ef932d5
Deepu Eswarath Ganapathiyadan <DGanapathiyadan@walmartlabs.com>
2018-01-05 16:10:09 -0800

    upgrade container version to 1.0.1, includes code-push support

--- app/src/main/java/com/walmartlabs/ern/showcase/MyApp.java
+++ app/src/main/java/com/walmartlabs/ern/showcase/MyApp.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.walmartlabs.electrode.reactnative.bridge.helpers.Logger;
 import com.walmartlabs.ern.container.ElectrodeReactContainer;
+import com.walmartlabs.ern.container.plugins.CodePushPlugin;
 
 public class MyApp extends Application {
 
@@ -11,7 +12,11 @@ public class MyApp extends Application {
     public void onCreate() {
         super.onCreate();
 
-        ElectrodeReactContainer.initialize(this, new ElectrodeReactContainer.Config().isReactNativeDeveloperSupport(true));
+        ElectrodeReactContainer.initialize(
+                this,
+                new ElectrodeReactContainer.Config().isReactNativeDeveloperSupport(true),
+                new CodePushPlugin.Config("LoNcxCcr-T6oPrXuZN1TFGUEvCQ7H1T1L1o7f").enableDebugMode(true));
+
         Logger.overrideLogLevel(Logger.LogLevel.DEBUG);
     }
 }
